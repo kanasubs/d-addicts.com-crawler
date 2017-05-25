5e39af6fd6182f666c0460a35e1fc1b71929e072
Carlos Fontes <ccfontes@gmail.com>
2017-05-25 02:27:06 +0100

    Fix delay code

--- core.py
+++ core.py
@@ -103,10 +103,10 @@ class Spider:
 
 class DAddictsSpider(AbstractSpider):
     def __init__(self, delay=None):
-        if delay is None: self.get_delay('http://www.d-addicts.com')
+        if delay is None: self._delay = self.get_delay('http://www.d-addicts.com')
         self.crawl = self.with_crawl_fn(self.extract_topic_links)
         self.topic_links = self.crawl("http://www.d-addicts.com/forums/page/subtitles#Japanese")
-        self.crawl = self.with_crawl_fn(self.extract_links_of_interest)
+        self.crawl = self.with_crawl_fn(self._extract_links_of_interest)
         self.file_link_store = FileLinkStore()
         self.delay = delay
 
@@ -144,7 +144,7 @@ class DAddictsSpider(AbstractSpider):
         return crawl_
 
     def __next__(self):
-        sleep(self.delay)
+        sleep(self._delay)
         links_to_files_of_interest = set()
         if self.topic_links:
             try:
